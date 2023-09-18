31637019496e1a07f4e48922cf412ee3be3dd64a
k98940 <pv1xxb@yandex.ru>
2023-09-18 18:42:48 +0600

    feat: changed ranger's design. (Mon, Sep 18, 2023 06:42:48 PM)

--- audio-player/js/state.js
+++ audio-player/js/state.js
@@ -10,7 +10,7 @@ export const playList =
       url: `${URL_PREFFIX}town.mp3`,
       name: 'Diablo II - Town',
       cover: `${COVER_PREFFIX}0`,
-      colorBaseColor: 'hsla(0, 51%, 24%, 0.635)',
+      colorBaseColor: '0, 51%, 24%',
       button: {
         url: '../assets/icons/btn-play-diablo.png',
         hue: '240deg',
@@ -21,7 +21,7 @@ export const playList =
       url: `${URL_PREFFIX}Floating Museum.mp3`,
       name: 'Ghost in the Shell - Floating Museum',
       cover: `${COVER_PREFFIX}1`,
-      colorBaseColor: 'hsla(180, 26%, 55%, 0.4)',
+      colorBaseColor: '180, 26%, 55%',
       button: {
         url: '../assets/icons/btn-play-ghost.png',
         hue: '170deg',
@@ -32,7 +32,7 @@ export const playList =
       url: `${URL_PREFFIX}Кудайберген Димаш – Знай.mp3`,
       name: 'Кудайберген Димаш – Знай',
       cover: `${COVER_PREFFIX}2`,
-      colorBaseColor: 'hsla(223, 30%, 45%, 0.4)',
+      colorBaseColor: '223, 30%, 45%',
       button: {
         url: '../assets/icons/btn-play-dimash.png',
         hue: '240deg',
@@ -43,7 +43,7 @@ export const playList =
       url: `${URL_PREFFIX}Vangelis - Prelude-audio.mp3`,
       name: 'Vangelis - Prelude',
       cover: `${COVER_PREFFIX}3`,
-      colorBaseColor: 'hsla(240, 8%, 51%, 0.4)',
+      colorBaseColor: '240, 8%, 51%',
       button: {
         url: '../assets/icons/btn-play-vangelis.png',
         hue: '280deg',
@@ -54,7 +54,7 @@ export const playList =
       url: `${URL_PREFFIX}Silent Hill 4 - Room of angel.mp3`,
       name: 'Silent Hill 4 - Room of angel',
       cover: `${COVER_PREFFIX}4`,
-      colorBaseColor: 'hsla(0, 0%, 59%, 0.4)',
+      colorBaseColor: '0, 0%, 59%',
       button: {
         url: '../assets/icons/btn-play-silenthill.png',
         hue: '45deg',
@@ -65,7 +65,7 @@ export const playList =
       url: `${URL_PREFFIX}Spirited Away  Reprise.mp3`,
       name: 'Spirited Away - Reprise',
       cover: `${COVER_PREFFIX}5`,
-      colorBaseColor: 'hsla(90, 53%, 63%, 0.4)',
+      colorBaseColor: '90, 53%, 63%',
       button: {
         url: '../assets/icons/btn-play-spirited-away.png',
         hue: '260deg',
